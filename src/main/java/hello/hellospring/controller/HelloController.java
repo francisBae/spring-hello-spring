ad905c0c2ebc51e829d3d000be10f67a863f0e0e
francisBae <qowhdgn@gmail.com>
2021-09-26 20:52:38 +0900

    feat: added api method and templates

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -29,6 +31,59 @@ public class HelloController {
         model.addAttribute("data","hello!!");
         return "hello";
     }
+
+    //템플릿 엔진
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){ //파라미터를 받겠다는 뜻(name이라는 파라미터)
+        //@RequestParam(value = "name",required=false) 으로 바꾸는 경우 name 파라미터 꼭 안 넘겨도됨
+        //default는 required=true 임
+        //required
+        model.addAttribute("name",name);
+
+        return "hello-template";
+    }
+
+    //API 방식 1 - 단순 문자열 return
+    //템플릿 엔진과 달리 뷰 없이 문자가 그대로 내려간다.
+    @GetMapping("hello-string")
+    @ResponseBody //html의 body태그가 아닌 http의 body에 이 데이터를 직접 넣어주겠다는 선언
+    public String helloString(@RequestParam("name")String name){
+        return "hello "+name; //ex. hello spring
+    }
+
+    /*
+        localhost:8080/hello-api 호출 시
+        1. 내장 톰캣 서버는 스프링에 이를 전달
+        2. 스프링 컨테이너에서 hello-api를 발견했으나 @ResponseBody 어노테이션이 붙어있음
+        3. 2의 어노테이션 없었으면 viewResolver를 동작시키나 @ResponseBody 어노테이션이 있는 상황
+        4. @ResponseBody가 있으면 Http body에 그대로 response 데이터를 넘김
+           문자열은 문자열로 반환하고, 객체는 json 방식으로 데이터 만들어서 반환(json이 디폴트)
+           - @ResponseBody는 HttpMessageConverter를 동작시킴
+             (HttpMessageConverter 내에 JsonConverter, StringConverter 포함됨)
+     */
+
+    //API 방식 2 - 데이터를 리턴하는 경우
+    //템플릿 엔진과 달리 뷰 없이 문자가 그대로 내려간다.
+    @GetMapping("hello-api")
+    @ResponseBody //html의 body태그가 아닌 http의 body에 이 데이터를 직접 넣어주겠다는 선언
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello; //객체를 return. json 형태로 리턴 {key : value}
+    }
+
+    static class Hello{
+        //A 클래스 내 static class B선언 시 A의 메소드에서 B 클래스 사용 가능
+        private String name;
+        //클래스 변수는 private으로 하고 getter setter 로 접근하는게 java bean 표준방식
+        public String getName(){
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    };
+
 
 
 }
