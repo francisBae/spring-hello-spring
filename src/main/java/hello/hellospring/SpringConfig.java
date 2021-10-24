12c35647fddd18b3a06e1b5f9dc520fed045e8de
francisBae <qowhdgn@gmail.com>
2021-10-24 19:58:21 +0900

    feat: Change repository(memory->jdbc)

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,25 +1,44 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 //Configuration, Bean 사용하면 컴포넌트 스캔, 자동의존관계 설정이 아닌
 //자바코드로 직접 스프린 빈 등록해서 사용 가능
 @Configuration
 public class SpringConfig {
 
+    private DataSource dataSource;
+
+    @Autowired //spring의 주입
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     //스프링빈에 등록하라는 걸로 스프링이 인식
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
+
     }
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
+        //손쉽게 memory repository에서 전환 가능
+        /*
+        OCP : Open-Closed Principle (개방-폐쇄 원칙 : 확장에는 열려있고, 수정 및 변경에는 닫혀 있다)
+
+         */
+
     }
 
 }
